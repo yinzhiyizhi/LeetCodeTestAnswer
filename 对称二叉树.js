65b92e5684a0c1a0bf88d1bcd7f42f531290967a
yinzhiyizhi <yinzhiyizhi@gmail.com>
2019-02-20 09:43:10 +0800

    Create 对称二叉树.js

---
+++ 对称二叉树.js
@@ -0,0 +1,50 @@
+/*给定一个二叉树，检查它是否是镜像对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+说明:
+
+如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+*/
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(root===null){
+        return true;
+    }
+    return symmetric(root.left,root.right);
+    
+    function symmetric(p,q){
+        if(p===null||q===null){
+            return p===q?true:false;
+        }
+        return p.val===q.val&&symmetric(p.left,q.right)&&symmetric(p.right,q.left);
+    }
+};
